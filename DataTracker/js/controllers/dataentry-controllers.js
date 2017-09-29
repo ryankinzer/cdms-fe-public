aaa1463a69fcbaacee1dd385db0c0526f91236dc
georgec <georgeclark@ctuir.org>
2017-09-29 11:30:38 -0700

    Made same change in datasheet controller.

--- DataTracker/js/controllers/dataentry-controllers.js
+++ DataTracker/js/controllers/dataentry-controllers.js
@@ -121,7 +121,7 @@ mod_de.controller('DataEntryDatasheetCtrl', ['$scope','$routeParams','DataServic
 		
 		//update our location options as soon as our project is loaded.
         $scope.$watch('project.Name', function(){
-        	if(!$scope.project) return;
+        	if(!$scope.project.Name) return;
 			
 			console.log("Inside watch project.Name...");
 			//console.log("$scope.project is next...");
