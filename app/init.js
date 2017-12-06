adf80d4c9a9405637c102b2cd0a203044545ff4c
Ken Burcham <kenburcham@gmail.com>
2017-12-06 09:54:26 -0800

    loading problems seem to be resolved + build.js to build a single js file

--- app/init.js
+++ app/init.js
@@ -58,7 +58,7 @@
                     setTimeout(function () {
                         angular.bootstrap(document.body, ['app']);
                         console.log("all systems GO!")
-                    }, 4000);
+                    }, 0);
                 });
                 
                 
--- build.js
+++ build.js
@@ -1,3 +1,12 @@
+//this file is the require optimization config file that can be used
+// to generate a single js source file that can be minified.
+
+//it requires npm and the r.js node package to be installed.
+// then here is how I run it:
+//
+// "c:\ctuir-data01\oit-gis\oit-gis-users\kenb\Application Data"\npm\r.js.cmd -o build.js
+//
+
  ({
 	baseUrl: "./app",
 	paths: {
@@ -15,5 +24,6 @@
 	},
 	name: "init",
 	out: "init-build.js",
+	optimize: "none"
 	
  })	
