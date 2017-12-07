257541498b4c3337eaa65fd5606a01a98cdc47c2
Ken Burcham <kenburcham@gmail.com>
2017-12-07 07:47:18 -0800

    add dataset to a project admin page

---
+++ app/core/admin/components/admin-page/admin-new-dataset.js
@@ -0,0 +1,45 @@
+﻿
+var admin_new_dataset = ['$scope', '$modal', 'DatasetService', 'AdminService', '$routeParams','ProjectService',
+    function ($scope, $modal, DatasetService, AdminService, $routeParams, ProjectService) {
+
+        $scope.datastore = DatasetService.getDatastore($routeParams.Id);
+        $scope.projects = ProjectService.getProjects();
+        $scope.datastoreFields = null; //the fields we'll load once the datastore is loaded
+
+        $scope.SelectedProject = null;
+
+        $scope.$watch('datastore.Id', function () {
+            if ($scope.datastore.Id > 0)
+                $scope.datastoreFields = AdminService.getMasterFields($scope.datastore.FieldCategoryId); //AdminService.getFields($routeParams.Id);
+        });
+
+        $scope.$watch('datastoreFields', function () {
+            if (!$scope.datastoreFields)
+                return;
+
+            angular.forEach($scope.datastoreFields, function (field) {
+                //parseField(field, $scope);
+                if (field.PossibleValues)
+                    field.Values = makeObjectsFromValues($scope.datastore.Id + field.DbColumnName, field.PossibleValues);
+
+            });
+
+
+        }, true);
+
+        $scope.saveField = function () {
+            $scope.saveResults = {};
+            //AdminService.saveMasterField($scope.SelectedField, $scope.saveResults);
+        }
+
+        $scope.selectField = function (field) {
+            $scope.SelectedField = field;
+        };
+
+        $scope.removeField = function (field) {
+            console.dir(field);
+            field.exclude = !field.exclude;
+        };
+
+    }
+];
