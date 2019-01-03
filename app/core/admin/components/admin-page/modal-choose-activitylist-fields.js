6f3f0005d1ffb6d69a53965e379cd059778999df
Ken Burcham <kenburcham@gmail.com>
2019-01-03 08:44:54 -0800

    fix: IE11 problems displaying field select modals

--- app/core/admin/components/admin-page/modal-choose-activitylist-fields.js
+++ app/core/admin/components/admin-page/modal-choose-activitylist-fields.js
@@ -5,6 +5,23 @@ var modal_admin_choose_activitylist_fields = ['$scope', '$uibModal','$uibModalIn
 
         $scope.savedActivityListFields = $scope.dataset.Config.ActivityListFields;
         
+        //this HeaderFields thing is a workaround for IE11 because it doesn't render the following. Better would be to just use this in the template:
+        /*
+               <select size="8" class="form-control" multiple ng-model="dataset.Config.DuplicateCheckFields">
+                    <option ng-repeat="field in dataset.Fields | filter:{FieldRoleId:1} | orderBy:'Label'" value="{{field.DbColumnName}}">{{field.Label}} {{field.Field.Units}}</option>
+                </select> 
+         */
+
+        $scope.HeaderFields = [];
+
+        $scope.dataset.Fields.forEach(function (field) { 
+            if (field.FieldRoleId == 1) {
+                field.FullLabel = field.Label + " (" + field.Units + ")";
+                $scope.HeaderFields.push(field);
+            }
+        });
+
+
         $scope.save = function () {
             $modalInstance.close();
         };
--- app/core/admin/components/admin-page/modal-choose-duplicate-fields.js
+++ app/core/admin/components/admin-page/modal-choose-duplicate-fields.js
@@ -4,7 +4,23 @@ var modal_admin_choose_duplicate_fields = ['$scope', '$uibModal','$uibModalInsta
     function ($scope, $modal, $modalInstance, AdminService) {
 
         $scope.savedDuplicateCheckFields = $scope.dataset.Config.DuplicateCheckFields;
-        
+
+        //this HeaderFields thing is a workaround for IE11 because it doesn't render the following. Better would be to just use this in the template:
+        /*
+               <select size="8" class="form-control" multiple ng-model="dataset.Config.DuplicateCheckFields">
+                    <option ng-repeat="field in dataset.Fields | filter:{FieldRoleId:1} | orderBy:'Label'" value="{{field.DbColumnName}}">{{field.Label}} {{field.Field.Units}}</option>
+                </select> 
+         */
+
+        $scope.HeaderFields = [];
+
+        $scope.dataset.Fields.forEach(function (field) { 
+            if (field.FieldRoleId == 1) {
+                field.FullLabel = field.Label + " (" + field.Units + ")";
+                $scope.HeaderFields.push(field);
+            }
+        });
+
         $scope.save = function () {
             $modalInstance.close();
         };
