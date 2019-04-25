e7e38ae91924bf0b7d5d750a9035d3970d1e0950
georgec <georgeclark@ctuir.org>
2019-04-25 08:46:48 -0700

    Updated code to avoid trying to process a null -- caused it to crash.

--- app/core/common/common-functions.js
+++ app/core/common/common-functions.js
@@ -1468,6 +1468,10 @@ function isDuplicateUploadFile(incoming_file, files_to_check) {
 //helper function that unJSON's the vals if a string or else returns the vals if already an object
 function getJsonObjects(vals) { 
 
+    // If a null was passed in, don't try to process it.
+    if (vals === null)
+        return vals;
+
     objvals = vals;
 
     try {
