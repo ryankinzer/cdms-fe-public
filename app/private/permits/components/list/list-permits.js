287fa26b02c5b54ea4f6f0e123a9a405145cef07
Ken Burcham <kenburcham@gmail.com>
2019-05-08 08:02:19 -0700

    initial permit module

---
+++ app/private/permits/components/list/list-permits.js
@@ -0,0 +1,34 @@
+﻿var list_permits = ['$scope', '$route', '$routeParams', '$uibModal', '$location', '$window', '$rootScope', 'PermitService', 'GridService',
+    function ($scope, $route, $routeParams, $modal, $location, $window, $rootScope, PermitService, GridService) {
+
+        $rootScope.inModule = "permits";
+
+        if (!$scope.Profile.hasRole("Permits"))
+            angular.rootScope.go("/unauthorized");
+
+        //$scope.currentPage = "Active";
+
+        $scope.permits = PermitService.getAllPermits();
+
+        $scope.permits.$promise.then(function () {
+            $scope.permitsGridDiv = document.querySelector('#active-permits-grid');
+            new agGrid.Grid($scope.permitsGridDiv, $scope.permitsGrid);
+        });
+
+        
+
+
+        $scope.permitsGrid = {
+            columnDefs: get,
+            rowData: $scope.permits,
+            rowSelection: 'multiple',
+            defaultColDef: {
+                editable: false,
+                sortable: true,
+                resizable: true,
+            },
+        }
+
+
+
+}];
---
+++ app/private/permits/permit-service.js
@@ -0,0 +1,36 @@
+﻿//Permits api
+
+permit_module.factory('AllPermits', ['$resource', function ($resource) {
+    return $resource(serviceUrl + '/api/v1/permit/allpermits', {}, {
+        query: { method: 'GET', params: {}, isArray: true }
+    });
+}]);
+
+
+
+
+
+
+permit_module.service('PermitService', ['$q',
+
+    'AllPermits',
+  
+    function ($q,
+       
+        AllPermits,
+      
+    ) {
+        var service = {
+
+            getAllPermits: function () {
+                return AllPermits.query();
+            },
+            
+        };
+
+        return service;
+
+    }
+]);
+
+
