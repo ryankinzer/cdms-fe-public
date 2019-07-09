adabb1632fed17c9d0f1643905a80f92a962d96e
georgec <georgeclark@ctuir.org>
2019-07-09 09:43:26 -0700

    Added Search page to the olc-module.

--- app/private/olc/olc-module.js
+++ app/private/olc/olc-module.js
@@ -6,12 +6,14 @@ require([
     'private/olc/components/events/modal-create-olc-subproject',
     'private/olc/components/events/events',
     'private/olc/components/events/modal-migrate-olc-event',
+    'private/olc/components/events/events-search',
 
 ], function () {
     olc_module.controller('ModalAddOlcEventCtrl', modal_add_olc_event);
     olc_module.controller('ModalCreateOlcSubprojectCtrl', modal_create_olc_subproject);
     olc_module.controller('OlcEventsCtrl', page_events);
     olc_module.controller('OlcMigrateEventCtrl', modal_migrate_olc_event);
+    olc_module.controller('OlcEventsSearchCtrl', events_search);
 });
 
 
