763d6e4d312f04711c856e036b5513bd9e77445d
Ken Burcham <kenburcham@gmail.com>
2019-07-25 09:18:57 -0700

    added includes polyfill to resolve IE map loading issue

--- app/core/common/common-functions.js
+++ app/core/common/common-functions.js
@@ -2182,3 +2182,19 @@ function formatUsPhone(phone) {
          return phone;
     }
 }
+
+//polyfill for includes in IE
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
