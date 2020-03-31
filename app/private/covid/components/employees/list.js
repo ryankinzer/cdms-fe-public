0cf0e15fb62e2681fc6e9535f536482ecdca2d0d
Ken Burcham <kenburcham@gmail.com>
2020-03-31 14:21:33 -0700

    covid app shows all dates through 4/30

--- app/private/covid/components/employees/list.js
+++ app/private/covid/components/employees/list.js
@@ -4,8 +4,9 @@ var covid_list = ['$scope', '$route', '$routeParams', '$uibModal', '$location', 
         $scope.employees = CovidService.getMyEmployees();
         $scope.work = CovidService.getMyEmployeesWork();
 
-        var today_text = moment(new Date()).format('M/D/YY');
-        var today = moment();
+        //note: they asked to show all dates through 4/30 instead of just through today... hence the redefinition of "today" :)
+        var today_text = moment(new Date('04/30/2020')).format('M/D/YY');
+        var today = moment(new Date('04/30/2020'));
         var begin = moment(new Date('03/15/2020'));
         $scope.lookup = {};
         $scope.save = {};
