7bdc385f6ac757f171237fcd866ca854125a4a63
georgec <georgeclark@ctuir.org>
2020-04-13 15:10:16 -0700

    Added code to show error, if CropAcres contains a nan value.

--- app/private/leasing/components/manage/add-production-modal.js
+++ app/private/leasing/components/manage/add-production-modal.js
@@ -3,6 +3,8 @@
 
         $scope.header_message = "Production";
 
+        $scope.saveResult = {};
+
         if (!$scope.production_modal) {
             $scope.production_modal = {
                 IncomeDate: $scope.currentDay,
@@ -13,7 +15,15 @@
 
         $scope.save = function () {
             console.dir($scope.production_modal);
-
+            
+            var intCropAcres = parseInt($scope.production_modal.CropAcres);
+            //if (($scope.production_modal.CropAcres !== 'undefined') && (intCropAcres === NaN))
+            if (isNaN($scope.production_modal.CropAcres))
+            {
+                $scope.saveResult.error = "Crop Acres must be either blank, or a number."
+                return;
+            }
+            //throw "Stopping right here...";
 
             $scope.production_modal.LeaseId = $scope.lease.Id;
             var save_result = LeasingService.saveProduction($scope.production_modal);
